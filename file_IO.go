b63bd28a49694ecf4c3452a6a24ca634ecf7586b
zipppzy <zipppzy939@gmail.com>
2022-09-20 20:21:36 -0500

    Added file reading and Instruction struct

---
+++ file_IO.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// reads text file and makes Instructions and adds them to the InstructionList
+func ReadBinary(filePath string) {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		InstructionList = append(InstructionList, Instruction{rawInstruction: scanner.Text()})
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+type Instruction struct {
+	rawInstruction string
+}
+
+var InstructionList []Instruction
+
 func main() {
 	fmt.Println("Hello World")
+	ReadBinary("addtest1_bin.txt")
+	fmt.Println(InstructionList)
 }
