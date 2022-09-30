903f87f073d8b80fd915fa857c578f7860a84b91
TobyOkoye <tobyuokoye@gmail.com>
2022-09-29 22:23:15 -0500

    Finished with the Command-Line. Currently working on printing out the Array.

--- team9_project1.go
+++ team9_project1.go
@@ -1,25 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Instruction struct {
-	rawInstruction  string
-	lineValue       uint64
-	memoryAddress   uint64
-	opcode          uint64
-	op              string
-	instructionType string
-	rm              uint8
-	shamt           uint8
-	rn              uint8
-	rd              uint8
+	rawInstruction string
+	lineValue      uint64
+	opcode         uint64
+	rm             uint8
+	shamt          uint8
+	rn             uint8
+	rd             uint8
 }
 
 var InstructionList []Instruction
 
 func main() {
-	ReadBinary("addtest1_bin.txt")
-	//fmt.Println(InstructionList)
-	ProcessInstructionList(InstructionList)
+	//Checks if addtest1_bin.txt exists
+	if _, err := os.Stat("addtest1_bin.txt"); err == nil {
+		os.Args[0] = "addtest1_bin.txt"
+	}
+
+	//Inputs Command-Line
+	ReadBinary(os.Args[0])
+
 	fmt.Println(InstructionList)
+	ProcessInstructionList(InstructionList)
+	//fmt.Println(InstructionList)
+
+	//Prints Array
+	for i := 0; i < len(InstructionList); i++ {
+		fmt.Println()
+	}
 }
